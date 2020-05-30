70d67175c00af5755424d5baabd4d487602e0421
yaasiin <yasinasshobry@gmail.com>
2020-05-30 17:27:36 +0700

    Add : - Top AppBar - CardView

---
+++ app/src/main/java/com/example/imagemachine/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.imagemachine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mToolbar = findViewById(R.id.topAppBar);
+        setSupportActionBar(mToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.top_app_bar, menu);
+        return true;
+    }
+}
